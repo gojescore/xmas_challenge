c3170f1718e0cdff8512b2e1d6b73fcde8788184
gojescore <apbeyer@hotmail.com>
2025-11-22 00:23:45 +0100

    Add files via upload

---
+++ public/minigames/grandprix.js
@@ -0,0 +1,7 @@
+// public/minigames/grandprix.js
+// Minimal placeholder mini-game for Nisse Grandprix
+
+export function renderGrandprix(challenge, api) {
+  api.setBuzzEnabled(true);
+  api.showStatus("🎵 Nisse Grandprix: Tryk STOP når I ved svaret!");
+}
