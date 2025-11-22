6a756bd42034edcfa8b670b131e62cd8a5531ef6
gojescore <apbeyer@hotmail.com>
2025-11-22 13:49:37 +0100

    Add files via upload

---
+++ public/data/deck/grandprix.js
@@ -0,0 +1,19 @@
+// public/data/deck/grandprix.js
+
+export const grandprixDeck = [
+  {
+    id: "gp1",
+    type: "Nisse Grandprix",
+    title: "Grandprix 1",
+    audioUrl: "https://ldaskskrbotxxhoqdzdc.supabase.co/storage/v1/object/public/grandprix-audio/hojtFraT.mp3",
+    used: false
+  },
+  {
+    id: "gp2",
+    type: "Nisse Grandprix",
+    title: "Grandprix 2",
+    audioUrl: "https://ldaskskrbotxxhoqdzdc.supabase.co/storage/v1/object/public/grandprix-audio/SorenBanjo.mp3",
+    used: false
+  }
+
+];
---
+++ public/data/deck/nissegaaden.js
@@ -0,0 +1,39 @@
+// public/data/deck/nissegaaden.js
+
+export const nisseGaaden = [
+  {
+    id: "ng1",
+    type: "NisseGåden",
+    title: "NisseGåden 1",
+    text: "Hvad går gennem byen uden at røre en eneste snefnug?",
+    used: false
+  },
+  {
+    id: "ng2",
+    type: "NisseGåden",
+    title: "NisseGåden 2",
+    text: "Hvad bliver vådere, jo mere det tørrer?",
+    used: false
+  },
+  {
+    id: "ng3",
+    type: "NisseGåden",
+    title: "NisseGåden 3",
+    text: "Jeg har mange tænder, men kan ikke bide. Hvad er jeg?",
+    used: false
+  },
+  {
+    id: "ng4",
+    type: "NisseGåden",
+    title: "NisseGåden 4",
+    text: "Jeg kan flyve uden vinger og græde uden øjne. Hvad er jeg?",
+    used: false
+  },
+  {
+    id: "ng5",
+    type: "NisseGåden",
+    title: "NisseGåden 5",
+    text: "Hvad kan du fange, men aldrig kaste?",
+    used: false
+  }
+];
