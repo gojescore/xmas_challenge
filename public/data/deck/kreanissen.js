13160977922a45c0897e022f0680303964a080f0
gojescore <apbeyer@hotmail.com>
2025-11-23 08:28:38 +0100

    Add files via upload

---
+++ public/data/deck/kreanissen.js
@@ -0,0 +1,23 @@
+// public/data/deck/kreanissen.js
+// KreaNissen photo-creation rounds (3 minutes each)
+
+export const DECK = [
+  {
+    id: "kn01",
+    type: "KreaNissen",
+    title: "KreaNissen 1",
+    text: "Lav den sjoveste nisse med ting i klassen. Tag et billede!",
+  },
+  {
+    id: "kn02",
+    type: "KreaNissen",
+    title: "KreaNissen 2",
+    text: "Byg en mini-juleby på bordet. Tag et billede!",
+  },
+  {
+    id: "kn03",
+    type: "KreaNissen",
+    title: "KreaNissen 3",
+    text: "Lav en kreativ julehat eller julefigur. Tag et billede!",
+  },
+];
