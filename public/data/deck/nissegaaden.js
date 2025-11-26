1e6acd2b4516217dab95ff5dc39b25ce1fb4decb
gojescore <apbeyer@hotmail.com>
2025-11-26 14:24:12 +0100

    Update nissegaaden.js

--- public/data/deck/nissegaaden.js
+++ public/data/deck/nissegaaden.js
@@ -1,11 +1,10 @@
-// public/data/deck/nissegaaden.js
-
 export const nisseGaaden = [
   {
     id: "ng1",
     type: "NisseGåden",
     title: "NisseGåden 1",
     text: "Hvad går gennem byen uden at røre en eneste snefnug?",
+    answer: "Vejen",
     used: false
   },
   {
@@ -13,6 +12,7 @@ export const nisseGaaden = [
     type: "NisseGåden",
     title: "NisseGåden 2",
     text: "Hvad bliver vådere, jo mere det tørrer?",
+    answer: "Et håndklæde",
     used: false
   },
   {
@@ -20,6 +20,7 @@ export const nisseGaaden = [
     type: "NisseGåden",
     title: "NisseGåden 3",
     text: "Jeg har mange tænder, men kan ikke bide. Hvad er jeg?",
+    answer: "En kam",
     used: false
   },
   {
@@ -27,6 +28,7 @@ export const nisseGaaden = [
     type: "NisseGåden",
     title: "NisseGåden 4",
     text: "Jeg kan flyve uden vinger og græde uden øjne. Hvad er jeg?",
+    answer: "En sky",
     used: false
   }
 ];
