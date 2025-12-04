2ad2c1b16f418db18569825b6a8d47f055b44d54
gojescore <apbeyer@hotmail.com>
2025-12-04 10:57:35 +0100

    Update billedequiz.js

--- public/data/deck/billedequiz.js
+++ public/data/deck/billedequiz.js
@@ -6,7 +6,7 @@ export const billedeQuizDeck = [
     type: "BilledeQuiz",
     title: "Billedquiz 1",
     text: "Hvad ser I på billedet?",
-    imageUrl: "/assets/billeder/santa-hat.png",
+    imageUrl: "/assets/billeder/bread.png",
     answer: "Nissehue",   // vises som facit på main-skærmen
     used: false
   },
@@ -15,9 +15,10 @@ export const billedeQuizDeck = [
     type: "BilledeQuiz",
     title: "Billedquiz 2",
     text: "Hvad foregår der på billedet?",
-    imageUrl: "/assets/billeder/snowman.png",
+    imageUrl: "/assets/billeder/butter.png",
     answer: "En snemand der står udenfor",
     used: false
   }
   // tilføj flere som du vil…
 ];
+
